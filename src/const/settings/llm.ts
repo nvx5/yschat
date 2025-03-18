6542d7984771d2e6c48d3f1ca1658af589d04d36
nvx5 <nilvaru@gmail.com>
2025-03-18 00:37:55 +0000

    Change default model and provider to AIML GPT-4o

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -20,7 +20,7 @@ export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
   },
 });
 
-export const DEFAULT_MODEL = 'meta-llama/llama-3.1-8b-instruct:free';
+export const DEFAULT_MODEL = 'gpt-4o';
 
 export const DEFAULT_EMBEDDING_MODEL = 'nomic-ai/nomic-embed-text-v1.5:free';
 export const DEFAULT_EMBEDDING_PROVIDER = ModelProvider.OpenRouter;
@@ -29,4 +29,4 @@ export const DEFAULT_RERANK_MODEL = 'rerank-english-v3.0';
 export const DEFAULT_RERANK_PROVIDER = 'cohere';
 export const DEFAULT_RERANK_QUERY_MODE = 'full_text';
 
-export const DEFAULT_PROVIDER = ModelProvider.OpenRouter;
+export const DEFAULT_PROVIDER = ModelProvider.AIML;
